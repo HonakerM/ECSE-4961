0d698f88dd3ab8088137f8341b1ef593b6c8bbca
Michael Honaker <mchonaker@gmail.com>
2022-01-18 16:05:54 -0500

    Added FL SIMD Operations

---
+++ project 1/main.cpp
@@ -0,0 +1,21 @@
+// General Imports
+#include <stdint.h>
+#include <stdio.h>
+
+//matrix imports
+#include "matrix.h"
+
+int main(int argc, char *argv[])
+{   
+    const float in_1[4] = {1,2,3, 4};
+    const float in_2[4] = {7, 9, 11, 13};
+
+    float output = fl_dot_product((const float *)&in_1, (const float *)&in_2, 4);
+    printf("%f\n",output);
+
+    struct fl_matrix matrix = fl_generate_matrix(2,2);
+    printf("%f %f\n",matrix.data[0][0], matrix.data[1][0]);
+    printf("%f %f\n",matrix.data[0][1], matrix.data[1][1]);
+
+}
+
