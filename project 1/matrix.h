467c1af6ea72592bcb5b80b7237d8e480a89264f
Michael Honaker <mchonaker@gmail.com>
2022-01-18 16:22:55 -0500

    Convert to C++ Class

--- project 1/matrix.h
+++ project 1/matrix.h
@@ -11,12 +11,28 @@
 #include <smmintrin.h>
 
 
-struct fl_matrix {
+class fl_matrix {
+public:
+    fl_matrix(float** data, uint width, uint height);
+    fl_matrix(uint width, uint height, bool fill_data);
+
+    uint get_num_rows();
+    uint get_num_columns();
+
+    const float* get_row(uint row);
+    const float* get_column(uint column);
+
+    void set_row(float* row, uint row_loc);
+    void set_column(float* column, uint column_loc);
+
     uint num_rows;
     uint num_columns;
-    float** data;
+    float ** data;
+
 };
 
-struct fl_matrix fl_generate_matrix(uint width, uint height);
-struct fl_matrix fl_mult_matrix(struct fl_matrix a, struct fl_matrix b);
+
+fl_matrix fl_mult_matrix(fl_matrix a, fl_matrix b);
+float* get_row(fl_matrix matrix, uint row);
+float* get_column(fl_matrix matrix, uint column);
 float fl_dot_product( const float* a, const float* b, int size);
