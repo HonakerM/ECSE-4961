44ddaf6ba9ae525134427d21650e433086d882d8
Michael Honaker <mchonaker@gmail.com>
2022-01-18 22:39:18 -0500

    Removed debugging statements from fl_matrix

--- project 1/main.cpp
+++ project 1/main.cpp
@@ -16,24 +16,11 @@ int main(int argc, char *argv[])
     float output = fl_simd_dot_product((const float *)&in_1, (const float *)&in_2, 4);
     printf("%f\n",output);
 
-    fl_matrix matrix_a = fl_matrix(10,10, true);
-    std::cout << "Matrix 1:" << std::endl;
-    std::cout << matrix_a;
+    fl_matrix matrix_a = fl_matrix(100,100, true);
 
-    std::cout << "Matrix 2:" << std::endl;
-    fl_matrix matrix_b = fl_matrix(10, 10, true);
-    std::cout << matrix_b;
+    fl_matrix matrix_b = fl_matrix(100, 100, true);
 
-
-    std::cout << "Output:" << std::endl;
     fl_matrix* output_mult = fl_simd_mult_matrix(&matrix_a, &matrix_b);
-    std::cout << *output_mult;
 
     delete output_mult;
-}
-
-
-fl_matrix* fl_test(fl_matrix* a, fl_matrix* b){
-    fl_matrix* matrix = new fl_matrix(10,10,false);
-    return matrix;
 }
