fee0b8e33cd39d983b7a4c4882c022fde54687ed
Michael Honaker <mchonaker@gmail.com>
2022-01-20 17:14:36 -0500

    Updated Main with better options

--- project 1/math.cpp
+++ project 1/math.cpp
@@ -1,11 +1,11 @@
 #include "math.h"
 
-
 matrix<float>* fl_simd_mult_matrix(matrix<float>* a, matrix<float>* b) {
     assert(a->get_num_columns() == b->get_num_rows());
     uint size = a->get_num_columns();
 
     matrix<float>* output = new matrix<float>(a->get_num_rows(), b->get_num_columns(), false); 
+
 
     for(uint i = 0; i < a->get_num_rows(); i ++ ){
         for(uint j = 0; j < b->get_num_columns(); j ++ ) {
@@ -49,7 +49,6 @@ float fl_simd_dot_product( const float* a, const float* b, uint size) {
         //if the size was adjusted and we're on the final dp
         // then create the new padded arrays
         if(size_adjust != 0 && i+4 >= size / 4){
-
             //generate new padded array
             adjusted_a = new float[4];
             adjusted_b = new float[4];
