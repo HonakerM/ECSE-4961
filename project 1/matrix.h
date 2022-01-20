52ef8c17348b9add0f8226c1202b7108657c812d
Michael Honaker <mchonaker@gmail.com>
2022-01-20 18:35:19 -0500

    Implemented Matrix Data Restructuring

--- project 1/matrix.h
+++ project 1/matrix.h
@@ -14,6 +14,16 @@
 #include <xmmintrin.h>
 #include <smmintrin.h>
 
+// define matrix organization macros
+// this determines weather a matrix is 
+// stored by rows or columns. This
+// is important in matrix multiplication
+// and cache hits. For example in AxB=C
+// you would want A to be organized by rows
+// and B to be organized by columns
+#define ROW_MAJOR 0x01
+#define COL_MAJOR 0x02
+
 //floating poitn matrix class
 template <typename T>
 class matrix {
@@ -23,6 +33,7 @@ public:
      */
     matrix(uint width, uint height, bool fill_data);
     ~matrix();
+    void free_matrix();
 
     /*
      * Accessors
@@ -30,6 +41,7 @@ public:
     uint get_num_rows() const;
     uint get_num_columns() const;
 
+    const T get_cell(uint row, uint column) const;
     const T* get_row(uint row) const;
     const T* get_column(uint column) const;
 
@@ -37,6 +49,7 @@ public:
      * Modifierts
      */
     void set_cell(T value, uint row_loc, uint column_loc);
+    void set_data_ordering(uint order);
 
     /*
      * Operator
@@ -47,9 +60,10 @@ private:
     uint num_rows;
     uint num_columns;
 
-    // see constructor for reasoning
-    T ** data_row_primary;
-    T ** data_column_primary;
+    // 
+    T ** data;
+    // default to column major
+    uint data_order = COL_MAJOR;
 
 };
 
@@ -59,7 +73,7 @@ private:
  */
 template <typename T> matrix<T>::matrix(uint width, uint height, bool fill_data){
     //generate array to hold all of the columns
-    data_column_primary = new T*[width];
+    data = new T*[width];
 
     //for each column
     for(uint i = 0; i < width; i++){
@@ -77,49 +91,27 @@ template <typename T> matrix<T>::matrix(uint width, uint height, bool fill_data)
         } 
 
         //assign column array to location
-        data_column_primary[i] = column;
+        data[i] = column;
     }
 
-
-    // duplicate the column table but with rows instead of columns
-    // this doubles the memory footprint of the application but 
-    // greatly speeds up getting arrays for SIMD multiplication
-    data_row_primary = new T*[height];
-
-    //for each row
-    for(uint i = 0; i < height; i++){
-        //generate array to hold each column for the row
-        float* row = new T[width];
-
-        //copy the data from the column matrix
-        for(uint j = 0; j < width; j++){
-            row[j] = data_column_primary[j][i];
-        } 
-
-        //asassign row array to locationsign 
-        data_row_primary[i] = row;
-    }
 
     //update general class values
     num_columns = width;
     num_rows = height;
 }
 
-template <typename T> matrix<T>::~matrix(){
+template <typename T>  matrix<T>::~matrix(){
+    free_matrix();
+}
+
+template <typename T> void matrix<T>::free_matrix() {
     // free all of the column arrays
     for(uint i = 0; i < get_num_columns(); i++){
-       delete[] data_column_primary[i];
-    }
-
-    // free all of the row arrays
-    for(uint i = 0; i < get_num_rows(); i++){
-       delete[] data_row_primary[i];
+       delete[] data[i];
     }
 
     // clear column array
-    delete[] data_column_primary;
-    delete[] data_row_primary;
-
+    delete[] data;
 }
 
 /*
@@ -133,29 +125,134 @@ template <typename T> uint matrix<T>::get_num_columns() const{
 }
 
 
+template <typename T> const T matrix<T>::get_cell(uint row, uint column) const {
+    // if data order is row major
+    if(data_order == ROW_MAJOR){
+        return data[row][column];
+    } else {
+        return data[column][row];
+    }
+}
+
 template <typename T> const T* matrix<T>::get_row(uint row) const{
-    return data_row_primary[row];
+    // if data order is row major
+    // directly access the array
+    if(data_order == ROW_MAJOR){
+        return data[row];
+
+    // else you have to generate the array. This
+    // should not be needed as the matrix should be
+    // correctly oriented. When this is used the data
+    // must be freed afterwards.
+    } else {
+        T* output_array = new T[get_num_columns()];
+        for(uint i=0;i<get_num_columns(); i++) {
+            output_array[i] = data[i][row];
+        }
+        return output_array;
+    }
 }
 
 
 template <typename T> const T* matrix<T>::get_column(uint column) const{
-    return data_column_primary[column];
-}
+    // if data order is column major
+    // directly access the array
+    if(data_order == COL_MAJOR){
+        return data[column];
+
+    // else you have to generate the array. This
+    // should not be needed as the matrix should be
+    // correctly oriented. When this is used the data
+    // must be freed afterwards.
+    } else {
+        T* output_array = new T[get_num_rows()];
+        for(uint i=0;i<get_num_rows(); i++) {
+            output_array[i] = data[i][column];
+        }
+        return output_array;
+    }}
 
 
 /* 
  * Modifiers
  */
 template <typename T> void matrix<T>::set_cell(T value, uint row_loc, uint column_loc){
-    data_column_primary[column_loc][row_loc] = value;
-    data_row_primary[row_loc][column_loc] = value;
+    //bounds checking
+    if(row_loc > get_num_rows() || column_loc > get_num_columns()){
+        return;
+    }
+
+    if(data_order == COL_MAJOR){
+        data[column_loc][row_loc] = value;
+    } else {
+        data[row_loc][column_loc] = value;
+    }
+}
+template <typename T> void matrix<T>::set_data_ordering(uint order){
+    //bounds checking
+    if(order != ROW_MAJOR || order != COL_MAJOR){
+        return;
+    }
+
+    //if order is already correct
+    if(data_order == order){
+        return;
+    }
+
+
+    if(order == ROW_MAJOR){
+        //generate array to hold all of the rows
+        T** new_data = new T*[get_num_rows()];
+
+        //for each column
+        for(uint i = 0; i < get_num_rows(); i++){
+            //generate array to hold each column for the row
+            T* row = new T[get_num_columns()];
+
+            for(uint j = 0; j < get_num_columns(); j++){
+                row[j] = get_cell(i,j);
+            } 
+
+            //assign row array to location
+            new_data[i] = row;
+        }
+
+        //free previous data
+        free_matrix();
+        data = new_data;
+
+    } else {
+        //generate array to hold all of the columns
+        T** new_data = new T*[get_num_columns()];
+
+        //for each column
+        for(uint i = 0; i < get_num_columns(); i++){
+            //generate array to hold each row for the column
+            T* column = new T[get_num_rows()];
+
+            for(uint j = 0; j < get_num_rows(); j++){
+                column[j] = get_cell(j, i);
+            } 
+
+            //assign column array to location
+            new_data[i] = column;
+        }
+
+        //free previous data
+        free_matrix();
+        data = new_data;
+    }
 }
 
 template <typename T> std::ostream& operator<<(std::ostream& os, const matrix<T>& matrix) {
     for(uint i = 0; i< matrix.get_num_rows(); i++){
 
         for(uint j =0; j< matrix.get_num_columns(); j++){
-            os << (T)matrix.data_column_primary[j][i] << " ";
+            if(matrix.data_order == COL_MAJOR){
+                os << (T)matrix.data[j][i] << " ";
+            } else {
+                os << (T)matrix.data[i][j] << " ";
+            }
         }
 
         os << "\n";
