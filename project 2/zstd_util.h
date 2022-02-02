1173881b0a803b42d62d7799e5e701f15137f2ea
Michael Honaker <mchonaker@gmail.com>
2022-02-01 20:23:52 -0500

    Continued ZSTD configuration

--- project 2/zstd_util.h
+++ project 2/zstd_util.h
@@ -2,6 +2,7 @@
 #define ZSTD_UTIL_H_
 
 #include <zstd.h>      // presumes zstd library is installed
+#include "main.h"
 
 /* 
  * Define
@@ -9,11 +10,42 @@
 #define CHUNK_BYTE_SIZE 16000
 
 
+
+
 /*
- * Functional Declerations
+ * Function Declerations
 */
 static void compress_chunk(void* const chunk, void* const comressed);
 
 
+
+/*! CHECK
+ *! Imported from zstd example
+ * Check that the condition holds. If it doesn't print a message and die.
+ */
+#define CHECK(cond, ...)                        \
+    do {                                        \
+        if (!(cond)) {                          \
+            fprintf(stderr,                     \
+                    "%s:%d CHECK(%s) failed: ", \
+                    __FILE__,                   \
+                    __LINE__,                   \
+                    #cond);                     \
+            fprintf(stderr, "" __VA_ARGS__);    \
+            fprintf(stderr, "\n");              \
+            exit(1);                            \
+        }                                       \
+    } while (0)
+
+/*! CHECK_ZSTD
+ *! Imported from zstd example
+ * Check the zstd error code and die if an error occurred after printing a
+ * message.
+ */
+#define CHECK_ZSTD(fn)                                           \
+    do {                                                         \
+        size_t const err = (fn);                                 \
+        CHECK(!ZSTD_isError(err), "%s", ZSTD_getErrorName(err)); \
+    } while (0)
 
 #endif 
