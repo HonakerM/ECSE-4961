e49280374b3f40f4244716409c41513171cade24
Michael Honaker <mchonaker@gmail.com>
2022-02-16 05:14:23 +0000

    Updated scripts

--- project 2/scripts/test.sh
+++ project 2/scripts/test.sh
@@ -6,9 +6,10 @@ make time
 
 for i in {1..15}
 do
-    echo "worker $i"
+    echo "worker $i" 
+    echo "worker $i" >> scripts/results.txt
     for v in {1..5}
     do
-        ./main.o $i ./scripts/test.data ./scripts/test.zstd > scripts/results.txt
+        ./main.o $i ./scripts/test.data ./scripts/test.zstd >> scripts/results.txt
     done
 done
